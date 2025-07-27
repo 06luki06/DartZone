9d640cf91438a0078bd1b61516ae8b66d6f39938
06luki06 <lukas.riegler2000@gmail.com>
2025-07-27 11:35:17 +0200

    Add first unit test  and update pipeline

---
+++ Scripts/run-tests.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Delete old TestResults directory...
+if exist "Tests\TestResults" (
+    rmdir /s /q "Tests\TestResults"
+)
+
+echo Running dotnet test...
+dotnet test --collect:"XPlat Code Coverage"
+if errorlevel 1 (
+    echo Tests failed.
+    exit /b %errorlevel%
+)
+
+echo Searching for coverage file...
+set "coverageFile="
+for /d %%D in (Tests\TestResults\*) do (
+    if exist "%%D\coverage.cobertura.xml" (
+        set "coverageFile=%%D\coverage.cobertura.xml"
+        goto found
+    )
+)
+
+:found
+if not defined coverageFile (
+    echo coverage.cobertura.xml not found!
+    exit /b 1
+)
+
+echo Coverage file found: %coverageFile%
+echo Generating HTML report...
+
+reportgenerator -reports:"%coverageFile%" -targetdir:"coveragereport" -reporttypes:Html
+
+echo Done. Report is located at coveragereport\index.html
---
+++ Tests/Shared/Dtos/Requests/UserRequestDtoTest.cs
@@ -0,0 +1,40 @@
+﻿using At.luki0606.DartZone.Shared.Dtos.Requests;
+using FluentAssertions;
+
+namespace At.luki0606.DartZone.Tests.Shared.Dtos.Requests
+{
+    [TestFixture]
+    public class UserRequestDtoTest
+    {
+        private UserRequestDto _userRequestDto;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userRequestDto = new();
+        }
+
+        [Test]
+        public void Ctor_ShallHaveDefaultValues()
+        {
+            _userRequestDto.Username.Should().BeEmpty();
+            _userRequestDto.Password.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Username_ShouldBeSettable()
+        {
+            const string username = "testuser";
+            _userRequestDto.Username = username;
+            _userRequestDto.Username.Should().Be(username);
+        }
+
+        [Test]
+        public void Password_ShouldBeSettable()
+        {
+            const string password = "testpassword";
+            _userRequestDto.Password = password;
+            _userRequestDto.Password.Should().Be(password);
+        }
+    }
+}
